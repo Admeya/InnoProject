dda562137a8d1168e5da3139862fab1d058f1326
Irina Bykova <ilugovova@neoflex.ru>
2017-02-08 20:56:42 +0300

    По регулярному выражению проверяем корректность вводимых чисел. Иначе бросаем исключение. Однако исключение прерывает только текущий поток.

--- src/main/java/ru/innopolis/itcources/lab1/RunThreads.java
+++ src/main/java/ru/innopolis/itcources/lab1/RunThreads.java
@@ -1,17 +1,5 @@
 package main.java.ru.innopolis.itcources.lab1;
 
-/**
- * Created by Ирина on 06.02.2017.
- * <p>
- * Необходимо разработать программу, которая получает на вход список ресурсов,
- * содержащих набор чисел и считает сумму всех положительных четных.
- * Каждый ресурс должен быть обработан в отдельном потоке, набор должен содержать лишь числа,
- * унарный оператор "-" и пробелы. Общая сумма должна отображаться на экране и изменяться в
- * режиме реального времени. Все ошибки должны быть корректно обработаны, все API покрыто модульными тестами
- * <p>
- * <p>
- * 1432
- */
 public class RunThreads extends Thread {
     private String fileNameIn;
     private int countThread;
@@ -22,6 +10,6 @@ public class RunThreads extends Thread {
     }
 
     public void run() {
-        SumCounting countSum = new SumCounting(fileNameIn, countThread);
+        new SumCounting(fileNameIn, countThread);
     }
 }
--- src/main/java/ru/innopolis/itcources/lab1/SumCounting.java
+++ src/main/java/ru/innopolis/itcources/lab1/SumCounting.java
@@ -3,6 +3,7 @@ package main.java.ru.innopolis.itcources.lab1;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 /**
  * Created by Ирина on 07.02.2017.
@@ -21,12 +22,11 @@ public class SumCounting {
         try {
             sc = new Scanner(file);
             while (sc.hasNextLine()) {
-                int nextNumber = sc.nextInt();
                 synchronized (this) {
-                    int tmpNumber = isPositiveAndEvenNumbers(nextNumber);
-                    if (tmpNumber > 0) {
-                        System.out.print("Work thread '" + countThread + "' sum = " + sum + " + " + tmpNumber + ". Sum = ");
-                        sum = sum + tmpNumber;
+                    int nextNumber = isNumberCorrect(sc.next());
+                    if (nextNumber > 0) {
+                        System.out.print("Work thread '" + countThread + "' sum = " + sum + " + " + nextNumber + ". Sum = ");
+                        sum = sum + nextNumber;
                         System.out.println(sum);
                     }
                 }
@@ -37,13 +37,22 @@ public class SumCounting {
         }
     }
 
-    public int isPositiveAndEvenNumbers(int num) {
-        if ((num % 2 == 0) && (num > 0)) {
-            System.out.println("Find positive and even number! This is number " + num + " in '" + this.countThread + "' thread");
-            return num;
+    public int isNumberCorrect(String number) {
+        int num = 0;
+        Pattern p = Pattern.compile("(^-\\d*$)|(^\\d*$)");
+        if (p.matcher(number).matches()) {
+            num = Integer.parseInt(number);
+
+            if ((num % 2 == 0) && (num > 0)) {
+                System.out.println("Find positive and even number! This is number " + num + " in '" + this.countThread + "' thread");
+                return num;
+            } else {
+                System.out.println("Number " + num + " in '" + this.countThread + "' thread is unsuitable for this example ");
+                num = 0;
+            }
         } else {
-            System.out.println("Number " + num + " in '" + this.countThread + "' thread is unsuitable for this example ");
-            num = 0;
+            throw new IllegalArgumentException("Yoooohooooohooo!!!! And a bottle with Rom!!! This is not correct number '" + number + "'");
+
         }
         return num;
     }
